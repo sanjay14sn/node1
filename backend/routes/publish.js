ae617db31ab6f629b83e57be9bec9c18bae8505c
sanjay14sn <snsanjay2002@gmail.com>
2025-04-14 02:35:48 +0530

    publish ride get api 1

--- backend/routes/publish.js
+++ backend/routes/publish.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { publishRide } = require('../controllers/publishcontroller');
+const publishController = require('../controllers/publishcontroller');
 
 // Route: POST /api/rides/publish
 router.post('/publish', publishRide);
